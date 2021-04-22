9a05b224d6f59c4230d2d51cef484b8d0b9fba56
Robert Ingrum <robert.ingrum@shopify.com>
2021-04-22 14:59:57 -0600

    Remove logging action call.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
-const actions = require("actions");
 const memory  = require("memory");
 const roles   = require("roles");
 const spawn   = require("spawn");
 const towers  = require("towers");
 
 module.exports.loop = function () {
-  actions.log();
-
   memory.clean()
   spawn.run();
   towers.run();
